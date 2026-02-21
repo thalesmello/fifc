08610740a39a86975e584330460a5e9a3cff44c1
Thales Mello <thalesmello@meta.com>
2026-02-20 16:00:36 -0800

    bugfix: Fix open opt

--- functions/_fifc_open_opt.fish
+++ functions/_fifc_open_opt.fish
@@ -1,14 +1,18 @@
 function _fifc_open_opt -d "Open a man page starting at the selected option"
     set -l cmd (string match --regex --groups-only -- '(^|\h+)(\w+) ?-*$' "$fifc_commandline")
     set -l opt (string trim --chars '\n ' -- "$fifc_candidate")
-    set -l regex "^\h*(-+[^\n]*)*$opt"
+    set -l regex "^[[:blank:]]*(-+.*)*$opt"
+
+    set -l output (man $cmd | col -b | string collect)
+
+    set -l linenr (echo $output | awk "/$regex/{print NR}")
 
     if type -q bat
-        man $cmd \
-            | bat --color=always --language man $fifc_bat_opts \
+        echo $output | bat --color=always --language man --style plain $fifc_bat_opts \
             # --RAW-CONTROL-CHARS allow color output of bat to be displayed
-            | less --RAW-CONTROL-CHARS --pattern "$regex"
+            | less --RAW-CONTROL-CHARS "+$linenr"
     else
-        man $cmd | less --pattern "$regex"
+        set -l regex "^[[:blank:]]*(-+.*)*$opt"
+        man $cmd | less "+$linenr"
     end
 end
