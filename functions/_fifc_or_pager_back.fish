70bd7aea97bd2dd854f30114c770e3a7c37398df
Thales Mello <thalesmello@meta.com>
2026-03-18 13:55:22 -0700

    fifc: Fix shift tab command used

--- functions/_fifc_or_pager_back.fish
+++ functions/_fifc_or_pager_back.fish
@@ -1,6 +1,6 @@
 function _fifc_or_pager_back
     if commandline --paging-mode
-        commandline --function backward-char
+        commandline --function complete-and-search
     else
         _fifc
     end
